c90646415c28100fed5faef026ec0828a3d64493
datzxje <tudabest12@gmail.com>
2024-08-13 19:36:18 +0700

    initial

---
+++ src/main/java/com/datzxje/hotelmanagement/entity/User.java
@@ -0,0 +1,25 @@
+package com.datzxje.hotelmanagement.entity;
+
+import com.datzxje.hotelmanagement.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private UserRole userRole;
+}
