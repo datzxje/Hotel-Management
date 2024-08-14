d6f71e9f8c620d950580d17397c77a9c5ec4fbdc
datzxje <tudabest12@gmail.com>
2024-08-14 16:22:16 +0700

    feature: confirm reservation, show bookings

--- src/main/java/com/datzxje/hotelmanagement/services/admin/reservation/ReservationServiceImpl.java
+++ src/main/java/com/datzxje/hotelmanagement/services/admin/reservation/ReservationServiceImpl.java
@@ -2,13 +2,18 @@ package com.datzxje.hotelmanagement.services.admin.reservation;
 
 import com.datzxje.hotelmanagement.dto.ReservationResponseDto;
 import com.datzxje.hotelmanagement.entity.Reservation;
+import com.datzxje.hotelmanagement.entity.Room;
+import com.datzxje.hotelmanagement.enums.ReservationStatus;
 import com.datzxje.hotelmanagement.repository.ReservationRepository;
+import com.datzxje.hotelmanagement.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,6 +21,8 @@ import java.util.stream.Collectors;
 public class ReservationServiceImpl implements ReservationService {
 
     private final ReservationRepository reservationRepository;
+
+    private final RoomRepository roomRepository;
 
     public static final int SEARCH_RESULT_PER_PAGE = 4;
 
@@ -33,5 +40,28 @@ public class ReservationServiceImpl implements ReservationService {
         reservationResponseDto.setTotalPages(reservationResponseDto.getTotalPages());
 
         return reservationResponseDto;
+    }
+
+    public boolean changeReservationStatus(Long id, String status) {
+        Optional<Reservation> optional = reservationRepository.findById(id);
+        if (optional.isPresent()) {
+            Reservation existedReservation = optional.get();
+
+            if(Objects.equals(status, "Approve")) {
+                existedReservation.setReservationStatus(ReservationStatus.APPROVED);
+            } else {
+                existedReservation.setReservationStatus(ReservationStatus.REJECTED);
+            }
+
+            reservationRepository.save(existedReservation);
+
+            Room existingRoom = existedReservation.getRoom();
+            existingRoom.setAvailable(false);
+
+            roomRepository.save(existingRoom);
+
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/datzxje/hotelmanagement/services/customer/booking/BookingService.java
+++ src/main/java/com/datzxje/hotelmanagement/services/customer/booking/BookingService.java
@@ -1,7 +1,11 @@
 package com.datzxje.hotelmanagement.services.customer.booking;
 
 import com.datzxje.hotelmanagement.dto.ReservationDto;
+import com.datzxje.hotelmanagement.dto.ReservationResponseDto;
 
 public interface BookingService {
+
     boolean postReservation(ReservationDto reservationDto);
+
+    ReservationResponseDto getAllReservationByUserId(Long userId, int pageNumber);
 }
